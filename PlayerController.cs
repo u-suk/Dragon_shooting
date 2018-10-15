7b685bb373d29598a396bc835ce9ef1abebca5c1
u-suk <34409132+u-suk@users.noreply.github.com>
2018-10-15 22:55:54 +0900

    カメラ移動
    カメラに直接引っ付けて動くプログラムを作成。しかしどちらかというとオブジェクトが回転するのに合わせてカメラの向きを変えたいが、それをする方法がいまいちわからない。

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Transform verRot;
+    private Transform horRot;
+
+    // Use this for initialization
+    void Start () {
+        verRot = transform.parent;
+        horRot = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Camera();
+    }
+    void Camera()
+    {
+        float X_Rotation = Input.GetAxisRaw("Mouse X");
+        float Y_Rotation = Input.GetAxisRaw("Mouse Y");
+        verRot.transform.Rotate(0, -X_Rotation, 0);
+        horRot.transform.Rotate(Y_Rotation, 0, 0);
+    }
+}
